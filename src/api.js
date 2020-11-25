d89849298cdd40f44865d4b32451983fcdd6a5ae
LHDEVELOPER <contato@lhdeveloper.me>
2020-11-25 13:25:45 -0300

    alterado BaseURL api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://api-relatorio-horas-me.umbler.net/'
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://api-relatorio-horas-me.umbler.net/'
+    // baseURL: 'http://localhost:3333'
 });
 
 api.interceptors.request.use(async (config) => {
