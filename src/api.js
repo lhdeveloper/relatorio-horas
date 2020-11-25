07a3fb1eb3bba590119386e97c49b99101b1d9b6
LHDEVELOPER <contato@lhdeveloper.me>
2020-11-25 01:54:44 -0300

    adicionado intercerptors axios

--- src/api.js
+++ src/api.js
@@ -4,4 +4,17 @@ const api = axios.create({
     baseURL: 'https://api-relatorio-horas-me.umbler.net/'
 });
 
+api.interceptors.request.use(async (config) => {
+    try {
+        const token = localStorage.getItem('app-token');
+
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }catch(err) {
+        console.log(err)
+    }
+})
+
 export default api;
