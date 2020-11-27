e6c60296c6b83cbe288435eb3d09f53c5d025638
LHDEVELOPER <contato@lhdeveloper.me>
2020-11-27 09:34:25 -0300

    ajustes visuais

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function Footer(){
     return (
         <footer className="bg-dark text-white py-3 py-sm-5">
-            <div className="container">
+            <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-6 col-12 copyright">
                         &copy; 2020 | Todos os direitos reservados.
                     </div>
-                    <div className="ml-sm-auto mx-auto mt-3 mt-sm-0">
-                        Dev by <a href="https://lhdeveloper.me" rel="noreferrer" title="Dev by LHDEVELOPER" target="_blank" className="dev-by"><span className="logo-rdp text-danger">&lt;LH/&gt;</span></a>
+                    <div className="col-sm-3 text-sm-right ml-sm-auto mt-3 mt-sm-0 dev">
+                        Dev by <a href="https://lhdeveloper.me" rel="noreferrer" title="Dev by LHDEVELOPER" target="_blank" className="dev-by">
+                            <span className="logo-rdp text-danger">&lt;LH/&gt;</span>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
                 <a className="navbar-brand border-right pr-4" href="/">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-alarm-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"/>
-                    </svg> Relatório de Horas
+                    </svg> <span className="d-none d-sm-block">Relatório de Horas</span>
                 </a>
                 <button className="d-none navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@ export default function Header() {
                     </ul>
                 </div>
 
-                <div className="dropdown justify-content-end mx-auto  mt-2 mt-sm-0 ml-sm-auto">
+                <div className="dropdown bg-red justify-content-end mt-2 mt-sm-0 ml-sm-auto">
                     <button className="btn btn-transparent text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
